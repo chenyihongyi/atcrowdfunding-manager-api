36adfd5fedff6973aa2959574f74536c8f4e8db8
chenyihong <chenyihongyi@163.com>
2019-01-21 22:27:29 +0800

    完成分配角色功能

--- src/main/java/com/atguigu/atcrowdfunding/manager/dao/UserMapper.java
+++ src/main/java/com/atguigu/atcrowdfunding/manager/dao/UserMapper.java
@@ -1,6 +1,9 @@
 package com.atguigu.atcrowdfunding.manager.dao;
 
+import com.atguigu.atcrowdfunding.bean.Role;
 import com.atguigu.atcrowdfunding.bean.User;
+import com.atguigu.atcrowfunding.vo.Data;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
@@ -27,4 +30,18 @@ public interface UserMapper {
 	List<User> queryList(Map<String, Object> paramMap);
 
 	Integer queryCount(Map<String, Object> paramMap);
+
+    //int deleteBatchUserByVO(Data data);
+
+    //int deleteBatchUserByVO(@Param("userList") List<User> userList);
+
+    int deleteBatchUserByVO(List<User> userList);
+
+    List<Role> queryAllRole();
+
+    List<Integer> queryRoleByUserid(Integer id);
+
+    int saveUserRoleRelationship(@Param("userid") Integer userid, @Param("data") Data data);
+
+    int deleteUserRoleRelationship(@Param("userid") Integer userid, @Param("data") Data data);
 }
--- src/main/java/com/atguigu/atcrowdfunding/manager/service/UserService.java
+++ src/main/java/com/atguigu/atcrowdfunding/manager/service/UserService.java
@@ -1,8 +1,11 @@
 package com.atguigu.atcrowdfunding.manager.service;
 
+import com.atguigu.atcrowdfunding.bean.Role;
 import com.atguigu.atcrowdfunding.bean.User;
 import com.atguigu.atcrowdfunding.util.Page;
+import com.atguigu.atcrowfunding.vo.Data;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,4 +28,14 @@ public interface UserService {
     int deleteUser(Integer id);
 
     int deleteBatchUser(Integer[] ids);
+
+    int deleteBatchUserByVO(Data data);
+
+    List<Role> queryAllRole();
+
+    List<Integer> queryRoleByUserid(Integer id);
+
+    int saveUserRoleRelationship(Integer userid, Data data);
+
+    int deleteUserRoleRelationship(Integer userid, Data data);
 }
